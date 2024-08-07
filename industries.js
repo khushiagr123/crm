603898b445447dc3f8109e8f9f3fb09e1a600b71
khushiagr123 <106469130+khushiagr123@users.noreply.github.com>
2024-08-07 18:14:32 +0530

    Update industries.js

--- industries.js
+++ industries.js
@@ -4,6 +4,37 @@ const industrySchema = mongoose.Schema({
   industryName: { type: String, required: true }  // e.g., Software, Manufacturing, Healthcare
 });
 
-const Industry = mongoose.model("Industry", industrySchema);
+const leadSchema = mongoose.Schema(
+  {
+    leadImage: { type: String },
+    leadOwner: { type: String, required: true },
+    firstName: { type: String, required: true },
+    title: { type: String },
+    phone: { type: String },
+    mobile: { type: String },
+    leadSource: { type: String },
+    industry: { type:  industrySchema  },
+    annualRevenue: { type: Number },
+    emailOptOut: { type: Boolean, default: false },
+    company: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    fax: { type: String },
+    website: { type: String },
+    leadStatus: { type: String },
+    numberOfEmployees: { type: Number },
+    rating: { type: String },
+    skypeID: { type: String },
+    secondaryEmail: { type: String },
+    twitter: { type: String },
+    address: { type: addressSchema, required: true },
+    description: { type: String },
+  },
+  {
+    timestamps: true, // Adds createdAt and updatedAt fields
+  }
+);
 
-export default Industry;
+const Lead = mongoose.model("Lead", leadSchema);
+
+export default Lead;
