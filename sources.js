b9551d71d78e4bc40b08cc45e7d8159704dd19f9
khushiagr123 <106469130+khushiagr123@users.noreply.github.com>
2024-08-07 18:15:26 +0530

    Update sources.js

--- sources.js
+++ sources.js
@@ -4,6 +4,37 @@ const sourceSchema = mongoose.Schema({
   sourceName: { type: String, required: true }  // e.g., Website, Referral, Advertisement
 });
 
-const Source = mongoose.model("Source", sourceSchema);
+const leadSchema = mongoose.Schema(
+  {
+    leadImage: { type: String },
+    leadOwner: { type: String, required: true },
+    firstName: { type: String, required: true },
+    title: { type: String },
+    phone: { type: String },
+    mobile: { type: String },
+    leadSource: { type: sourceSchema },
+    industry: { type: String },
+    annualRevenue: { type: Number },
+    emailOptOut: { type: Boolean, default: false },
+    company: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    fax: { type: String },
+    website: { type: String },
+    leadStatus: { type: String },
+    numberOfEmployees: { type: Number },
+    rating: { type: String },
+    skypeID: { type: String },
+    secondaryEmail: { type: String },
+    twitter: { type: String },
+    address: { type: addressSchema, required: true },
+    description: { type: String },
+  },
+  {
+    timestamps: true, // Adds createdAt and updatedAt fields
+  }
+);
 
-export default Source;
+const Lead = mongoose.model("Lead", leadSchema);
+
+export default Lead;
