bba7f3e291ce170b264805e12d2ad929f100584d
khushiagr123 <106469130+khushiagr123@users.noreply.github.com>
2024-08-07 18:12:01 +0530

    Update companies.js

--- companies.js
+++ companies.js
@@ -8,6 +8,37 @@ const companySchema = mongoose.Schema({
   industryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Industry', required: true }
 });
 
-const Company = mongoose.model("Company", companySchema);
+const leadSchema = mongoose.Schema(
+  {
+    leadImage: { type: String },
+    leadOwner: { type: String, required: true },
+    firstName: { type: String, required: true },
+    title: { type: String },
+    phone: { type: String },
+    mobile: { type: String },
+    leadSource: { type: String },
+    industry: { type: String },
+    annualRevenue: { type: Number },
+    emailOptOut: { type: Boolean, default: false },
+    company: { type: companySchema, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    fax: { type: String },
+    website: { type: String },
+    leadStatus: { type: String },
+    numberOfEmployees: { type: Number },
+    rating: { type: String },
+    skypeID: { type: String },
+    secondaryEmail: { type: String },
+    twitter: { type: String },
+    address: { type: addressSchema, required: true },
+    description: { type: String },
+  },
+  {
+    timestamps: true, // Adds createdAt and updatedAt fields
+  }
+);
 
-export default Company;
+const Lead = mongoose.model("Lead", leadSchema);
+
+export default Lead;
